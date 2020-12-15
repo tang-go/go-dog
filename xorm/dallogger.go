b2cb5e6283202c39be0bf1152f5839275948b03b
tangjie <tangjiework@outlook.com>
2020-12-15 17:02:15 +0800

    xorm 日志

--- xorm/dallogger.go
+++ xorm/dallogger.go
@@ -1,99 +1,77 @@
 package xorm
 
 import (
-	"github.com/tang-go/go-dog/log"
-	"xorm.io/core"
+	mylog "github.com/tang-go/go-dog/log"
+	"xorm.io/xorm/log"
 )
 
-// Logger is the default implment of core.ILogger
+// Logger is a logger interface
 type Logger struct {
-	level   core.LogLevel
+	level   log.LogLevel
 	showSQL bool
 }
 
-//NewLogger 初始化日志
-func NewLogger(level core.LogLevel, showSQL bool) *Logger {
-	l := new(Logger)
+//Debug Debug
+func (l *Logger) Debug(v ...interface{}) {
+	mylog.Debugln(v...)
+}
+
+//Debugf Debugf
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	mylog.Debugf(format, v...)
+}
+
+//Error Error
+func (l *Logger) Error(v ...interface{}) {
+	mylog.Errorln(v...)
+}
+
+//Errorf Errorf
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	mylog.Errorf(format, v...)
+}
+
+//Info Info
+func (l *Logger) Info(v ...interface{}) {
+	mylog.Infoln(v...)
+}
+
+//Infof Infof
+func (l *Logger) Infof(format string, v ...interface{}) {
+	mylog.Infof(format, v...)
+}
+
+//Warn Warn
+func (l *Logger) Warn(v ...interface{}) {
+	mylog.Warnln(v...)
+}
+
+//Warnf Warnf
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	mylog.Warnf(format, v...)
+}
+
+//Level Level
+func (l *Logger) Level() log.LogLevel {
+	return l.level
+}
+
+//SetLevel SetLevel
+func (l *Logger) SetLevel(level log.LogLevel) {
 	l.level = level
-	l.showSQL = showSQL
-	return l
 }
 
-// Error implement core.ILogger
-func (s *Logger) Error(v ...interface{}) {
-	log.Errorln(v...)
-	return
-}
-
-// Errorf implement core.ILogger
-func (s *Logger) Errorf(format string, v ...interface{}) {
-	log.Errorf(format, v...)
-	return
-}
-
-// Debug implement core.ILogger
-func (s *Logger) Debug(v ...interface{}) {
-	log.Debugln(v...)
-	return
-}
-
-// Debugf implement core.ILogger
-func (s *Logger) Debugf(format string, v ...interface{}) {
-	if !s.showSQL {
-		return
+//ShowSQL ShowSQL
+func (l *Logger) ShowSQL(show ...bool) {
+	l.showSQL = true
+	for _, s := range show {
+		if s == false {
+			l.showSQL = false
+		}
 	}
-	if s.level > core.LOG_DEBUG {
-		return
-	}
-	log.Debugf(format, v...)
-	return
 }
 
-// Info implement core.ILogger
-func (s *Logger) Info(v ...interface{}) {
-	log.Debugln(v...)
-	return
-}
-
-// Infof implement core.ILogger
-func (s *Logger) Infof(format string, v ...interface{}) {
-	log.Infof(format, v...)
-	return
-}
-
-// Warn implement core.ILogger
-func (s *Logger) Warn(v ...interface{}) {
-	log.Warnln(v...)
-	return
-}
-
-// Warnf implement core.ILogger
-func (s *Logger) Warnf(format string, v ...interface{}) {
-	log.Warnf(format, v...)
-	return
-}
-
-// Level implement core.ILogger
-func (s *Logger) Level() core.LogLevel {
-	return s.level
-}
-
-// SetLevel implement core.ILogger
-func (s *Logger) SetLevel(l core.LogLevel) {
-	s.level = l
-	return
-}
-
-// ShowSQL implement core.ILogger
-func (s *Logger) ShowSQL(show ...bool) {
-	if len(show) == 0 {
-		s.showSQL = true
-		return
-	}
-	s.showSQL = show[0]
-}
-
-// IsShowSQL implement core.ILogger
-func (s *Logger) IsShowSQL() bool {
-	return s.showSQL
+//IsShowSQL IsShowSQL
+func (l *Logger) IsShowSQL() bool {
+	return l.showSQL
 }
