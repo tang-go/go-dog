b9eebb21bbc67f77de63b59ba2a6f9692c789264
tangjie05 <tangjie05@baidu.com>
2021-07-30 15:58:30 +0800

    跨域问题

--- pkg/gateway/gateway.go
+++ pkg/gateway/gateway.go
@@ -530,7 +530,7 @@ func (g *Gateway) cors() gin.HandlerFunc {
 
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Headers", "Content-Type,TraceID, IsTest, Token,TimeOut")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
